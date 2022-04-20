7d5fe509f5babffbf6479103c3099116f506471b
Hédl Petra <hedlpetra12@gmail.com>
2022-04-20 13:40:39 +0200

    Creating the interfaces

---
+++ Ambrus/Ambrus.Model/Interfaces/IVisitor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambrus.Model.Interfaces
+{
+    public interface IVisitor
+    {
+        void Visit(Player player);
+
+        void Visit(Enemy enemy);
+
+        void Visit(Projectile projectile);
+    }
+}
