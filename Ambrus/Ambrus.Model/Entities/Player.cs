980f37e7e5ea07e949b54ca29f9cb702574f29a7
Hédl Petra <hedlpetra12@gmail.com>
2022-04-20 13:39:40 +0200

    Creating the game entities

---
+++ Ambrus/Ambrus.Model/Entities/Player.cs
@@ -0,0 +1,65 @@
+﻿using Ambrus.Model.Entities;
+using Ambrus.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Point = System.Windows.Point;
+using Size = System.Windows.Size;
+
+namespace Ambrus.Model
+{
+    public class Player : GameEntity, IVisitable
+    {
+        public static readonly Size Size = new Size(113, 116);
+
+        private Action<int> playerHit;
+
+        private Point centerFront;
+        
+        public Player(Rect area, int health, Action<int> playerHit)
+            : base(area, SpriteType.Player, Enums.Team.Player, health)
+        {
+            this.centerFront = new Point((area.X + (area.Width / 2)), area.Y);
+            this.playerHit = playerHit;
+            this.ReloadDuration = 10;
+            this.ReloadTime = 0;
+            this.MoveSpeed = 2;
+
+            this.IsRigid = true;
+            this.DieOnCollision = false;
+            this.DamageCollision = 0;
+        }
+
+        public int ReloadDuration { get; set; }
+
+        public int ReloadTime { get; set; }
+
+        public Point CenterFront
+        {
+            get { return this.centerFront; }
+            private set { this.centerFront = value; }
+        }
+
+        public override void Move(Vector dir)
+        {
+            base.Move(dir);
+            this.centerFront += dir;
+        }
+
+        public override void TakeDamage(int amount)
+        {
+            base.TakeDamage(amount);
+            this.playerHit.Invoke(this.Health);
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
+
---
+++ Ambrus/Ambrus.Model/Entities/Projectile.cs
@@ -0,0 +1,51 @@
+﻿using Ambrus.Model.Entities;
+using Ambrus.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Ambrus.Model
+{
+    public class Projectile : GameEntity, IVisitable
+    {
+        private static System.Windows.Size size;
+
+        public Projectile(Rect area, SpriteType sprite, Vector facingDir, Enums.Team team)
+            : base(area, sprite, team, 1)
+        {
+            this.FacingDirection = facingDir;
+
+            if (team == Enums.Team.Player)
+            {
+                Size = new System.Windows.Size(20, 39);
+            }
+            else
+            {
+                Size = new System.Windows.Size(20, 56);
+            }
+
+            this.MoveSpeed = 8;
+
+            this.IsRigid = false;
+            this.DieOnCollision = true;
+            this.DamageCollision = 1;
+        }
+
+        public static System.Windows.Size Size
+        {
+            get { return new System.Windows.Size(20, 39); }
+            set { size = value; }
+        }
+
+        public Vector FacingDirection { get; private set; }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
