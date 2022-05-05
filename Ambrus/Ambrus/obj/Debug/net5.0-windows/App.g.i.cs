29fddb54166235679140ae94e970b7d2f0eb941f
Aldeed1234 <thomas369258@gmail.com>
2022-05-05 21:04:42 +0200

    wpf

--- Ambrus/Ambrus/obj/Debug/net5.0-windows/App.g.i.cs
+++ Ambrus/Ambrus/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Ambrus {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Ambrus {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public static void Main() {
             Ambrus.App app = new Ambrus.App();
             app.InitializeComponent();
